6013d686b3dcfb04a6a7fe9e68b2942efc7d4cbc
Neil Henegan <neil@platform45.com>
2014-07-04 15:01:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo "Checking if command line tools are installed..."
 if [[ -z `which gcc` || -z `gcc -v 2>&1 | grep LLVM` ]]; then
   echo "Installing OSX Command Line tools"
   xcode-select --install
-  read -p " ####--  Follow the dialoge and hit [Enter] to once installed ---####"
+  read -p " ####--  Follow the dialoge and hit [Enter] once installed ---####"
 else
   echo "...Command Line Tools good to go :)"
 fi
